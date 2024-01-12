8e45eda520a1e08c423a60a123ded8b6a85e6a34
Nordgaren <nordgarentv@gmail.com>
2024-01-12 14:40:36 -0700

    Update patch_section_headers

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@
 use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::mem::size_of;
 use std::ops::Range;
 use indicatif::ProgressIterator;
 use pe_util::PE;
 use crate::windows::api::GetLastError;
 use crate::windows::consts::MEM_FREE;
-use crate::windows::structs::IMAGE_DOS_HEADER;
-use crate::windows::structs::{IMAGE_NT_HEADERS, IMAGE_SECTION_HEADER};
 
 mod process;
 mod windows;
@@ -123,33 +120,14 @@ fn dump_buffer(path: &str, buffer: Vec<u8>) {
 }
 
 unsafe fn patch_section_headers(buffer: Vec<u8>) -> Vec<u8> {
-    let buffer_ptr = buffer.as_ptr() as usize;
+    let pe = PE::from_slice(&buffer[..]).expect("Could not read buffer as PE.");
 
-    // Figure out the offset in the buffer to the NT header
-    let nt_header_offset = (*(buffer_ptr as *const IMAGE_DOS_HEADER)).e_lfanew as usize;
-
-    // Read the NT header to figure out how sections we have and how much RVA's + sizes so we
-    // can determine the offset to the section headers;
-    let nt_header = &*((buffer_ptr + nt_header_offset) as *const IMAGE_NT_HEADERS);
-    let section_count = nt_header.FileHeader.NumberOfSections;
-    let rva_count = nt_header.OptionalHeader.NumberOfRvaAndSizes;
-
-    // Locate the section headers. Rust's IMAGE_NT_HEADERS64 assumes a fixed 16 RVA's but this might
-    // be different in reality so we take care of the situation where it's less by manually
-    // adjusting the offset.
-    let section_base = nt_header_offset
-        + size_of::<IMAGE_NT_HEADERS>()
-        + ((rva_count - 16) * 8) as usize;
-
-    let section_header_size = size_of::<IMAGE_SECTION_HEADER>();
-    for i in 0..section_count {
-        let section_header_offset = section_base + section_header_size * i as usize;
-        let section_header = (buffer_ptr + section_header_offset) as *mut IMAGE_SECTION_HEADER;
-
+    let sections = pe.section_headers_mut();
+    for sections in sections {
         // Since we're dumping from memory we need to correct the PointerToRawData and SizeOfRawData
         // such that analysis tools can locate the sections again.
-        (*section_header).SizeOfRawData = (*section_header).Misc.VirtualSize;
-        (*section_header).PointerToRawData = (*section_header).VirtualAddress;
+        sections.SizeOfRawData = sections.Misc.VirtualSize;
+        sections.PointerToRawData = sections.VirtualAddress;
     }
 
     buffer
